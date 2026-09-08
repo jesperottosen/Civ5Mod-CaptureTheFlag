21f3db3692be9333d1f7dbaa9b976a3f3c15cbd7
Jesper Ottosen <10980509+jesperottosen@users.noreply.github.com>
2026-09-08 19:53:34 +0200

    1.2 onCreateFirstCity

---
+++ Capture The Flag Solution/Capture The Flag/CTF_onCreateFirstCity.lua
@@ -0,0 +1,33 @@
+-- CTF_CreateFlagUnits
+-- Author: yepzer
+-- DateCreated: 9/8/2026 4:46:21 PM
+--------------------------------------------------------------
+
+print ("Loaded OK")
+
+function onCreateFirstCity(iPlayer, cityX, cityY) 
+
+	-- init
+	if (iPlayer == nil) then return end
+	if (Players[iPlayer] == nil) then return end
+	if (Players[iPlayer]:IsMinorCiv()) then return end
+	if (cityX == nil) then return end
+	if (cityY == nil) then return end
+
+	-- if one city
+	if (Players[iPlayer]:GetNumCities() == 1) then
+		print("one city")
+
+		-- create unit, if max not reached
+		pPlayer = Players[iPlayer]
+		local canTrain = pPlayer:CanTrain(GameInfo.Units["UNIT_OWNFLAG"].ID, true, true, true, false)
+		if (canTrain) then			
+			pPlayer:InitUnit(GameInfo.Units["UNIT_OWNFLAG"].ID, cityX, cityY)
+		end
+	end
+
+end 
+
+--------------------------------------------------------------
+
+GameEvents.PlayerCityFounded.Add( onCreateFirstCity )
